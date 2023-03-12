e4faeeedb731af68ae29f963dffb2483bf957f4a
B. Gorchev <bgorchev@aiopsgroup.com>
2023-03-12 12:33:03 +0200

    changes in 18+ minimum abet amount

--- src/components/Card/MinimumAmount/MinimumAmount.jsx
+++ src/components/Card/MinimumAmount/MinimumAmount.jsx
@@ -1,7 +1,7 @@
 const MinimumAmount = () => {
   return (
     <>
-      <p className="text-[0.563rem]  text-center text-primary opacity-[1] tracking-[-0.09px] md:tracking-[-0.11px] col-span-2 md:col-span-3 md:text-[0.688rem] md:text-left md:text-graySecondary xl:col-span-3 xl:text-[12px]">
+      <p className="text-[0.563rem] xl:ml-[-1rem]  text-center text-primary opacity-[1] tracking-[-0.09px] md:tracking-[-0.11px] col-span-2 md:col-span-3 md:mb-[-0.938rem] md:text-[0.688rem] md:text-left md:text-graySecondary xl:col-span-3 xl:text-[12px]">
         <span className="font-bold">18+ </span> - Minimum bet amount - 100 -
         Some other important information Terms&Conditions text -{" "}
         <span className="font-bold">Terms&ConditionsLink.com</span>
